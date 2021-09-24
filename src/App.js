dc4ad7666b8f286299724db5a511a96acd86fc45
borshon420 <borshonarafat420@gmail.com>
2021-09-25 01:37:53 +0600

    create-react-core-concepts

--- src/App.js
+++ src/App.js
@@ -2,24 +2,82 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nayoks = ['Rubel', 'BappaRaz', 'Koober', 'Bappi', 'Shuvo', 'Omar Sani'];
+  const cinemas = [
+    
+      {nayok: 'Rubel', nayika: 'Moushumi'},
+      {nayok: 'BappaRaz', nayika: 'Cheka'},
+      {nayok: 'Koober', nayika: 'Kopila'},
+      {nayok: 'Joshim', nayika: 'Shuchorita'}
+
+    
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    <ul>
+        {
+        nayoks.map(nayok => <li>{nayok}</li>)
+        }
+    </ul>
+    {/* <Cinema nayok="J Bond" nayika="Cat W"></Cinema> */}
+      {
+        cinemas.map(cinema => <Cinema nayok={cinema.nayok} nayika={cinema.nayika}></Cinema>)
+      }
     </div>
   );
+}
+
+
+/* 
+<Person name={nayoks[0]} nayika="Moushumi"></Person>
+      <Person name={nayoks[1]} nayika="Cheka"></Person>
+      <Person name={nayoks[2]} nayika="Kopila"></Person>
+*/
+
+/* 
+<Friend number="01734" address="Noakhali"></Friend>
+      <Friend number="01678" address="Rongpur"></Friend>
+*/
+
+/* 
+      <Person name="Rubel" nayika="Moushumi"></Person>
+      <Person name="BappaRaz" nayika="Cheka"></Person>
+      <Person name="Kuber" nayika="Kopila"></Person>
+*/
+
+function Person(props) {
+  console.log(props)
+  const person = {
+    backgroundColor: 'skyblue',
+    border: '3px solid lightsalmon',
+    borderRadius: '10px',
+    margin: '10px'
+  }
+  return (
+    <div style={person}>
+      <h1>Name: {props.name}</h1>
+      <h3>Hero of: {props.nayika}</h3>
+    </div>
+  ); 
+}
+
+function Friend(props) {
+  return (
+    <div className="Person">
+      <h3>Phone: {props.number}</h3>
+      <h5>Address: {props.address}</h5>
+    </div>
+  )
+}
+
+function Cinema(props){
+  return (
+    <div className="Person">
+      <h2>Nayok: {props.nayok}</h2>
+      <h5>Nayika: {props.nayika}</h5>
+    </div>
+  )
 }
 
 export default App;
